090efd20ffe34acb4d18590f8e314a99c96f6842
dayuan <geniusew@gmail.com>
2012-12-18 18:44:05 +0800

    update

--- server/install_config.sh
+++ server/install_config.sh
@@ -2,9 +2,9 @@
 # 直接执行即可 bash <(curl -s https://raw.github.com/Bigcircle/config/master/server/install_config.sh)
 
 # 该脚本用于导入.bash_profile和.vimrc文件到服务器上
-[ -f ~/.bash_profile ] && mv ~/.bash_profile ~/.bash_profile.save
-curl -#O https://raw.github.com/Bigcircle/config/master/server/.bash_profile
-source ~/.bash_profile
+[ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.save
+curl -#O https://raw.github.com/Bigcircle/config/master/server/.bashrc
+source ~/.bashrc
 
 [ -f ~/.vimrc ] && rm -f ~/.vimrc
 cd ~/ && curl -#O https://raw.github.com/Bigcircle/config/master/server/.vimrc
